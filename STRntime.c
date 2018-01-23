80b0095d9f02c2a8255bdd3a7e7e37ebc294656c
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-01-23 10:55:21 +0000

    Create STRntime.c

---
+++ STRntime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,c;
+	char s[100];
+            scanf("%d",&c);
+	gets(s);
+	
+	for(i=1;i<=c;i++)
+	{
+		
+		printf("%s",s);	
+	}
+
+	
+	// your code goes here
+	return 0;
+}
